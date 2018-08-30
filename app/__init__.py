841c522cb86b397f2ab2160c40107ec8861f3a19
freshprincekla <robinahkasoma@Robinahs-iMac.fios-router.home>
2018-08-30 17:31:02 -0400

    Refactored to reduce duplications

--- app/__init__.py
+++ app/__init__.py
@@ -61,31 +61,21 @@ def Adjacents(u):
         if v[0] >= 1 and v[0] < N and v[1] >= 1 and v[1] < N: adj.append(v)
     return adj
 
+assignment = {
+    'a':1,
+    'b':2,
+    'c':3,
+    'd':4,
+    'e':5,
+    'f':6,
+    'g':7,
+    'h':8
+}
+
 def convertToNum(letter):
-    assignment = {
-        'a':1,
-        'b':2,
-        'c':3,
-        'd':4,
-        'e':5,
-        'f':6,
-        'g':7,
-        'h':8
-    }
     return assignment[letter]
 
 def convertToLetter(num):
-    assignment = {
-        'a':1,
-        'b':2,
-        'c':3,
-        'd':4,
-        'e':5,
-        'f':6,
-        'g':7,
-        'h':8
-    }
-    #num = num + 1
     for key, value in assignment.items():
         if num == value:
             return key
